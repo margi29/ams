9d38f7bf7b761e94ea6a3ac862bfc60fa7c92ae8
aarya073 <aaryaba03@gmail.com>
2025-03-03 23:28:03 +0530

    Backend server
    Added files for backend server connection

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const ConnectMongo = require('./DB') 
+const Users = require('./model')
+
+require('dotenv').config()
+
+const app = express();
+
+// to read request body
+app.use(express.json())
+
+
+ConnectMongo()
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
+
+// getting user data
+app.get("/", async (req, res) => {
+    try {
+      const data = await Users.find({});
+      res.send(data);
+    } catch (error) {
+      res.send(error);
+    }
+  });
+  
+  // posting user data
+  app.post("/", async (req, res) => {
+    try {
+      const payload = req.body; //Payload - data send from the frontend
+  
+      const User = new Users(payload);
+      await User.save();
+  
+      res.status(200).json({ status: "success" });
+    } catch (error) {
+      res.send(error);
+    }
+  });
+  
+  app.listen(process.env.PORT, () => {
+    console.log(`server is up and running on port ${process.env.PORT}`);
+  });
