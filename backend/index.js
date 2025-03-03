1b5d8dc87687a902339eef52bcc487468711b3e7
margi29 <margivadodaria@gmail.com>
2025-03-04 00:37:28 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const ConnectMongo = require('./DB') 
-const Users = require('./model')
+// const Users = require('./model')
 
 require('dotenv').config()
 
 const app = express();
 
 // to read request body
-app.use(express.json())
+// app.use(express.json())
 
 
 ConnectMongo()
@@ -27,19 +27,19 @@ app.get("/", async (req, res) => {
   });
   
   // posting user data
-  app.post("/", async (req, res) => {
-    try {
-      const payload = req.body; //Payload - data send from the frontend
+  // app.post("/", async (req, res) => {
+  //   try {
+  //     const payload = req.body; //Payload - data send from the frontend
   
-      const User = new Users(payload);
-      await User.save();
+  //     const User = new Users(payload);
+  //     await User.save();
   
-      res.status(200).json({ status: "success" });
-    } catch (error) {
-      res.send(error);
-    }
-  });
+  //     res.status(200).json({ status: "success" });
+  //   } catch (error) {
+  //     res.send(error);
+  //   }
+  // });
   
-  app.listen(process.env.PORT, () => {
-    console.log(`server is up and running on port ${process.env.PORT}`);
-  });
+  // app.listen(process.env.PORT, () => {
+  //   console.log(`server is up and running on port ${process.env.PORT}`);
+  // });
